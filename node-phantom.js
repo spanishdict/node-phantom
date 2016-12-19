388d33ef3eca6f23b96fb98cf83b493f91bcd2f1
Kenan Shifflett <kenan@spanishdict.com>
2016-12-19 10:18:19 -0800

    Wait for server listen.

--- node-phantom.js
+++ node-phantom.js
@@ -64,160 +64,161 @@ module.exports={
 					window.socket = socket;\n\
 				};\n\
 			</script></head><body></body></html>');
-		}).listen();
+		}).listen(function() {
 
-		var port=server.address().port;
-		spawnPhantom(port,function(err,phantom){
-			if(err){
-				server.close();
-				callback(true);
-				return;
-			}
-			var pages={};
-			var cmds={};
-			var cmdid=0;
-			function request(socket,args,callback){
-				args.splice(1,0,cmdid);
-	//			console.log('requesting:'+args);
-				socket.emit('cmd',JSON.stringify(args));
+			var port=server.address().port;
+			spawnPhantom(port,function(err,phantom){
+				if(err){
+					server.close();
+					callback(true);
+					return;
+				}
+				var pages={};
+				var cmds={};
+				var cmdid=0;
+				function request(socket,args,callback){
+					args.splice(1,0,cmdid);
+		//			console.log('requesting:'+args);
+					socket.emit('cmd',JSON.stringify(args));
 
-				cmds[cmdid]={cb:callback};
-				cmdid++;
-			}
+					cmds[cmdid]={cb:callback};
+					cmdid++;
+				}
 
-			var io=socketio.listen(server,{'log level':1});
+				var io=socketio.listen(server,{'log level':1});
 
-			io.sockets.on('connection',function(socket){
-				socket.on('res',function(response){
-	//				console.log(response);
-					var id=response[0];
-					var cmdId=response[1];
-					switch(response[2]){
-					case 'pageCreated':
-						var pageProxy={
-							open:function(url, callback){
-								if(callback === undefined){
-									request(socket, [id, 'pageOpen', url]);
-								}else{
-									request(socket, [id, 'pageOpenWithCallback', url], callback);
+				io.sockets.on('connection',function(socket){
+					socket.on('res',function(response){
+		//				console.log(response);
+						var id=response[0];
+						var cmdId=response[1];
+						switch(response[2]){
+						case 'pageCreated':
+							var pageProxy={
+								open:function(url, callback){
+									if(callback === undefined){
+										request(socket, [id, 'pageOpen', url]);
+									}else{
+										request(socket, [id, 'pageOpenWithCallback', url], callback);
+									}
+								},
+								release:function(callback){
+									request(socket,[id,'pageRelease'],callbackOrDummy(callback));
+								},
+								render:function(filename,callback){
+									request(socket,[id,'pageRender',filename],callbackOrDummy(callback));
+								},
+								renderBase64:function(extension,callback){
+									request(socket,[id,'pageRenderBase64',extension],callbackOrDummy(callback));
+								},
+								injectJs:function(url,callback){
+									request(socket,[id,'pageInjectJs',url],callbackOrDummy(callback));
+								},
+								includeJs:function(url,callback){
+									request(socket,[id,'pageIncludeJs',url],callbackOrDummy(callback));
+								},
+								sendEvent:function(event,x,y,callback){
+									request(socket,[id,'pageSendEvent',event,x,y],callbackOrDummy(callback));
+								},
+								uploadFile:function(selector,filename,callback){
+									request(socket,[id,'pageUploadFile',selector,filename],callbackOrDummy(callback));
+								},
+								evaluate:function(evaluator,callback){
+									request(socket,[id,'pageEvaluate',evaluator.toString()].concat(Array.prototype.slice.call(arguments,2)),callbackOrDummy(callback));
+								},
+								set:function(name,value,callback){
+									request(socket,[id,'pageSet',name,value],callbackOrDummy(callback));
+								},
+								get:function(name,callback){
+									request(socket,[id,'pageGet',name],callbackOrDummy(callback));
+								},
+								setFn: function(pageCallbackName, fn, callback) {
+									request(socket, [id, 'pageSetFn', pageCallbackName, fn.toString()], callbackOrDummy(callback));
 								}
-							},
-							release:function(callback){
-								request(socket,[id,'pageRelease'],callbackOrDummy(callback));
-							},
-							render:function(filename,callback){
-								request(socket,[id,'pageRender',filename],callbackOrDummy(callback));
-							},
-							renderBase64:function(extension,callback){
-								request(socket,[id,'pageRenderBase64',extension],callbackOrDummy(callback));
-							},
-							injectJs:function(url,callback){
-								request(socket,[id,'pageInjectJs',url],callbackOrDummy(callback));
-							},
-							includeJs:function(url,callback){
-								request(socket,[id,'pageIncludeJs',url],callbackOrDummy(callback));
-							},
-							sendEvent:function(event,x,y,callback){
-								request(socket,[id,'pageSendEvent',event,x,y],callbackOrDummy(callback));
-							},
-							uploadFile:function(selector,filename,callback){
-								request(socket,[id,'pageUploadFile',selector,filename],callbackOrDummy(callback));
-							},
-							evaluate:function(evaluator,callback){
-								request(socket,[id,'pageEvaluate',evaluator.toString()].concat(Array.prototype.slice.call(arguments,2)),callbackOrDummy(callback));
-							},
-							set:function(name,value,callback){
-								request(socket,[id,'pageSet',name,value],callbackOrDummy(callback));
-							},
-							get:function(name,callback){
-								request(socket,[id,'pageGet',name],callbackOrDummy(callback));
-							},
-							setFn: function(pageCallbackName, fn, callback) {
-								request(socket, [id, 'pageSetFn', pageCallbackName, fn.toString()], callbackOrDummy(callback));
-							}
-						};
+							};
 
-                        // Add page proxy.
-                        pages[id] = pageProxy;
-						cmds[cmdId].cb(null,pageProxy);
-						delete cmds[cmdId];
-						break;
-					case 'phantomExited':
-						request(socket,[0,'exitAck']);
-						server.close();
-						cmds[cmdId].cb();
-						delete cmds[cmdId];
-						break;
-					case 'pageJsInjected':
-					case 'jsInjected':
-						cmds[cmdId].cb(JSON.parse(response[3])===true ? null : true);
-						delete cmds[cmdId];
-						break;
-					case 'pageOpened':
-						if(cmds[cmdId]!==undefined){	//if page is redirected, the pageopen event is called again - we do not want that currently.
-							if(cmds[cmdId].cb !== undefined){
-								cmds[cmdId].cb(null, response[3]);
-							}
+	                        // Add page proxy.
+	                        pages[id] = pageProxy;
+							cmds[cmdId].cb(null,pageProxy);
 							delete cmds[cmdId];
+							break;
+						case 'phantomExited':
+							request(socket,[0,'exitAck']);
+							server.close();
+							cmds[cmdId].cb();
+							delete cmds[cmdId];
+							break;
+						case 'pageJsInjected':
+						case 'jsInjected':
+							cmds[cmdId].cb(JSON.parse(response[3])===true ? null : true);
+							delete cmds[cmdId];
+							break;
+						case 'pageOpened':
+							if(cmds[cmdId]!==undefined){	//if page is redirected, the pageopen event is called again - we do not want that currently.
+								if(cmds[cmdId].cb !== undefined){
+									cmds[cmdId].cb(null, response[3]);
+								}
+								delete cmds[cmdId];
+							}
+							break;
+						case 'pageRenderBase64Done':
+							cmds[cmdId].cb(null,response[3]);
+							delete cmds[cmdId];
+							break;
+						case 'pageGetDone':
+						case 'pageEvaluated':
+							cmds[cmdId].cb(null,JSON.parse(response[3]));
+							delete cmds[cmdId];
+							break;
+						case 'pageReleased':
+							delete pages[id]; // fallthru
+						case 'pageSetDone':
+						case 'pageJsIncluded':
+						case 'pageRendered':
+						case 'pageEventSent':
+						case 'pageFileUploaded':
+	                    case 'clearedCookies':
+	                    case 'enabledCookies':
+							cmds[cmdId].cb(null);
+							delete cmds[cmdId];
+							break;
+						default:
+							console.error('got unrecognized response:'+response);
+							break;
 						}
-						break;
-					case 'pageRenderBase64Done':
-						cmds[cmdId].cb(null,response[3]);
-						delete cmds[cmdId];
-						break;
-					case 'pageGetDone':
-					case 'pageEvaluated':
-						cmds[cmdId].cb(null,JSON.parse(response[3]));
-						delete cmds[cmdId];
-						break;
-					case 'pageReleased':
-						delete pages[id]; // fallthru
-					case 'pageSetDone':
-					case 'pageJsIncluded':
-					case 'pageRendered':
-					case 'pageEventSent':
-					case 'pageFileUploaded':
-                    case 'clearedCookies':
-                    case 'enabledCookies':
-						cmds[cmdId].cb(null);
-						delete cmds[cmdId];
-						break;
-					default:
-						console.error('got unrecognized response:'+response);
-						break;
-					}
+					});
+					socket.on('push', function(request) {
+						var id = request[0];
+						var cmd = request[1];
+						var callback = callbackOrDummy(pages[id] ? pages[id][cmd] : undefined);
+						callback(unwrapArray(request[2]));
+					});
+
+	                // Browser proxy.
+					var proxy={
+						createPage:function(callback){
+							request(socket,[0,'createPage'],callbackOrDummy(callback));
+						},
+						injectJs:function(filename,callback){
+							request(socket,[0,'injectJs',filename],callbackOrDummy(callback));
+						},
+						exit:function(callback){
+							request(socket,[0,'exit'],callbackOrDummy(callback));
+						},
+						clearCookies:function(callback){
+							request(socket,[0,'clearCookies'],callbackOrDummy(callback));
+						},
+						enableCookies:function(enable,callback){
+							request(socket,[0,'enableCookies',enable],callbackOrDummy(callback));
+						}
+					};
+
+	                // Save a reference to proxy for clean up.
+	                phanta.push(proxy);
+
+	                // Return browser proxy.
+	                callback(null,proxy);
 				});
-				socket.on('push', function(request) {
-					var id = request[0];
-					var cmd = request[1];
-					var callback = callbackOrDummy(pages[id] ? pages[id][cmd] : undefined);
-					callback(unwrapArray(request[2]));
-				});
-
-                // Browser proxy.
-				var proxy={
-					createPage:function(callback){
-						request(socket,[0,'createPage'],callbackOrDummy(callback));
-					},
-					injectJs:function(filename,callback){
-						request(socket,[0,'injectJs',filename],callbackOrDummy(callback));
-					},
-					exit:function(callback){
-						request(socket,[0,'exit'],callbackOrDummy(callback));
-					},
-					clearCookies:function(callback){
-						request(socket,[0,'clearCookies'],callbackOrDummy(callback));
-					},
-					enableCookies:function(enable,callback){
-						request(socket,[0,'enableCookies',enable],callbackOrDummy(callback));
-					}
-				};
-
-                // Save a reference to proxy for clean up.
-                phanta.push(proxy);
-
-                // Return browser proxy.
-                callback(null,proxy);
 			});
 		});
 	}
